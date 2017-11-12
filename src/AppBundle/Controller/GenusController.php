5049cf27a284548f5268e75bf441c03a4cd6d3dd
pseudoanime <luckilaxmi@gmail.com>
2017-11-12 01:13:15 +0000

    json response

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class GenusController extends Controller
@@ -13,11 +15,24 @@ class GenusController extends Controller
      */
     public function indexAction(Request $request, $a)
     {
-        $notes = ['123', '456', '789'];
+        // $notes = ['123', '456', '789'];
 
         return $this->render('genus/show.html.twig', [
             'name'  => $a,
-            'notes' => $notes,
+            // 'notes' => $notes,
         ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction(Request $request)
+    {
+        $notes = ['123', '456', '789'];
+        $data  =[
+            'notes' => $notes, ];
+
+        return new JsonResponse($data);
     }
 }
